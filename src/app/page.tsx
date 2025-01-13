c1a7fb5cbf9334a1e1466d0f347daa48d9303dfa
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2025-01-13 15:02:17 -0300

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,15 +57,16 @@ export default function Home() {
         ctaLink="https://www.eventbrite.com/e/1135200829239?aff=oddtdtcreator"
         videoSrc="/videos/kudalini_hero.mp4"
       />
-      <TestimonialsSection
-        onActionClick={scrollToCTASection}
-      />
+
       <ProblemSolutionSection
         problemText="Many people struggle with feelings of disconnection and emotional blocks, preventing them from experiencing true harmony and fulfillment."
         solutionText="Through Kundalini Activation, we awaken the dormant energy within you, bringing balance, healing, and a deeper connection with yourself and the world around you."
       />
       <PhotoGallery />
       <BenefitsSection benefits={benefits} />
+      <TestimonialsSection
+        onActionClick={scrollToCTASection}
+      />
       <FAQSection />
       <LocationSection />
       <CTASection
